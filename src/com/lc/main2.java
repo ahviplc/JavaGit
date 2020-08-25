d0d7f85a8a54eb473987520335b1bfcf9361ae9b
LC <ahlc@sina.cn>
2020-08-25 23:03:08 +0800

    commit 4.

--- src/com/lc/main2.java
+++ src/com/lc/main2.java
@@ -4,5 +4,6 @@ public class main2 {
     public static void main(String[] args) {
         System.out.println("git 2");
         System.out.println("git 3");
+        System.out.println("git 4");
     }
 }
