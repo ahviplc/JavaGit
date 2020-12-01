299d8b4a5df15e9827f4a8b13f21e91dd98a2097
viplj <302699185@qq.com>
2020-12-01 16:16:53 +0800

    回滚git 1的提交

--- src/com/lc/main2.java
+++ src/com/lc/main2.java
@@ -2,7 +2,6 @@ package com.lc;
 
 public class main2 {
     public static void main(String[] args) {
-        System.out.println("git 1");
         System.out.println("git 2");
         System.out.println("git 3");
         System.out.println("git 4");
