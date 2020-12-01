03a80d70c1d02aafb0f4128cde7dc27ff37a672a
viplj <302699185@qq.com>
2020-12-01 16:07:30 +0800

    添加git 1

--- src/com/lc/main2.java
+++ src/com/lc/main2.java
@@ -2,6 +2,7 @@ package com.lc;
 
 public class main2 {
     public static void main(String[] args) {
+        System.out.println("git 1");
         System.out.println("git 2");
         System.out.println("git 3");
         System.out.println("git 4");
